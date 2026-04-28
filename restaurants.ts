36ea01635402c549389c21572d0b42dd09d9f81b
petrueshka <olesja.petrova@gmail.com>
2026-04-28 12:18:18 +0200

    Create restaurants.ts

---
+++ restaurants.ts
@@ -0,0 +1,42 @@
+export default [
+  {
+    name: 'Silver Rice Sushi 🍣',
+    priceBracket: 2,
+    deliveryTimeMinutes: 40,
+    openHour: '12',
+    closeHour: '23',
+    distance: '5',
+  },
+  {
+    name: 'Nikko\'s Rotisserie Chicken 🍗',
+    priceBracket: 1,
+    deliveryTimeMinutes: 20,
+    openHour: '12',
+    closeHour: '21',
+    distance: '8',
+  },
+  {
+    name: 'Aita Trattoria 🍝',
+    priceBracket: 3,
+    deliveryTimeMinutes: 60,
+    openHour: '18',
+    closeHour: '22',
+    distance: '1',
+  },
+  {
+    name: 'Lula Bagel 🥯',
+    priceBracket: 1,
+    deliveryTimeMinutes: 20,
+    openHour: '0',
+    closeHour: '12',
+    distance: '2',
+  },
+  {
+    name: 'Golden Chopstick 🥢',
+    priceBracket: 1,
+    deliveryTimeMinutes: 20,
+    openHour: '15',
+    closeHour: '23',
+    distance: '12',
+  },
+];
